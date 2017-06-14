4a654e2778ce3c74a7c5a4b1f67ce5e7e334dd91
Carlos Roig <roigcarlo@gmail.com>
2017-06-14 17:36:40 +0200

    Soft tabs...

--- build/configure.sh
+++ build/configure.sh
@@ -11,10 +11,10 @@ rm -rf CMakeFiles\ 2> /dev/null
 export CC=clang
 export CXX=clang++
 
-cmake ..									                                                      															          \
--DCMAKE_C_COMPILER=${CC}                                                                 				                        \
--DCMAKE_CXX_COMPILER=${CXX}																																	                            \
--DCMAKE_CXX_FLAGS="${CMAKE_CXX_FLAGS} -msse3 -fopenmp=libomp -Wsign-compare -std=c++11 -Wno-overloaded-virtual -O3"		  \
--DCMAKE_C_FLAGS="${CMAKE_C_FLAGS} -msse3 -fopenmp=libomp -O3"								                                            \
--DKRATOS_PATH="/home/roigcarlo/Kratos/"                                                                                 \
+cmake ..                                                                                                            \
+-DCMAKE_C_COMPILER=${CC}                                                                                            \
+-DCMAKE_CXX_COMPILER=${CXX}                                                                                         \
+-DCMAKE_CXX_FLAGS="${CMAKE_CXX_FLAGS} -msse3 -fopenmp=libomp -Wsign-compare -std=c++11 -Wno-overloaded-virtual -O3" \
+-DCMAKE_C_FLAGS="${CMAKE_C_FLAGS} -msse3 -fopenmp=libomp -O3"                                                       \
+-DKRATOS_PATH="/home/roigcarlo/Kratos/"                                                                             \
 -DCMAKE_BUILD_TYPE=Release
