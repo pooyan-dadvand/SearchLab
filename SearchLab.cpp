069705ac0979190c6d9ebb324b4426cc3dfc931a
MiguelPasenau <miguel@cimne.upc.edu>
2017-10-24 18:21:07 +0200

    verify that there are points

--- SearchLab.cpp
+++ SearchLab.cpp
@@ -1,17 +1,17 @@
-//System includes
+// System includes
+#include <cstdlib>
 #include <fstream>
+#include <iomanip>
 #include <iostream>
 #include <string>
-#include <cstdlib>
-#include <iomanip>
 
 #ifdef USE_KRATOS
-	// Ugly fixes
-	#include <assert.h>
-	#define KRATOS_ERROR std::cout
+// Ugly fixes
+#include <assert.h>
+#define KRATOS_ERROR std::cout
 
-	#include "interfaces/points_old_interface.h"
-	#include "interfaces/objects_old_interface.h"
+#include "interfaces/points_old_interface.h"
+#include "interfaces/objects_old_interface.h"
 #endif
 
 // Containers
@@ -20,180 +20,195 @@
 // Interfaces
 #include "interfaces/points_new_interface.h"
 
-#ifdef USE_KRATOS
-	#include "interfaces/points_old_interface.h"
-	#include "interfaces/objects_old_interface.h"
-#endif
+int RunPointSearchComparison( std::string Filename, double Radius ) {
 
-int RunPointSearchComparison(std::string Filename, double Radius) {
+  // Input data
+  std::cout << std::setprecision( 4 ) << std::fixed;
 
-	// Input data
-	std::cout << std::setprecision(4) << std::fixed;
-
-	Point ** points;
+  Point **points;
   Point point;
-  SphereObject<3> object;
+  SphereObject< 3 > object;
 
-	std::ifstream input;
-	input.open(Filename.c_str());
+  std::ifstream input;
+  input.open( Filename.c_str() );
 
-	if (!input) {
-		std::cout << "Cannot open data file" << std::endl;
-		return 1;
-	}
+  if ( !input ) {
+    std::cout << "Cannot open data file" << std::endl;
+    return 1;
+  }
 
-	std::cout << "Comparison for " << Filename << std::endl;
+  std::cout << "Comparison for " << Filename << std::endl;
 
-	std::size_t npoints;
+  std::size_t npoints;
 
-	input >> npoints;
+  input >> npoints;
 
-	points = new Point*[npoints];
-  std::vector<Entities::PtrObjectType> objects(npoints);
+  if ( npoints <= 0) {
+    std::cout << "ERROR: no points information read, nothing to do!!!" << std::endl;
+    return 1;
+  }
+  
+  points = new Point *[ npoints ];
+  std::vector< Entities::PtrObjectType > objects( npoints );
 
-	std::size_t pid;
+  std::size_t pid;
 
-	for(std::size_t i = 0; i < npoints; i++) {
-		input >> pid;
-		input >> point;
+  for ( std::size_t i = 0; i < npoints; i++ ) {
+    input >> pid;
+    input >> point;
 
-    for(std::size_t d = 0; d < 3; d++) {
-      object[d] = point[d];
+    for ( std::size_t d = 0; d < 3; d++ ) {
+      object[ d ] = point[ d ];
     }
-    object.radius = 0.5/npoints;
+    object.radius = 0.5 / npoints;
 
-		points[i] = new Point(point);
-		points[i]->id = pid;
+    points[ i ] = new Point( point );
+    points[ i ]->id = pid;
 
-    objects[i] = new SphereObject<3>(object);
-    objects[i]->id = pid;
-    objects[i]->radius = 0.5/npoints;
-	}
+    objects[ i ] = new SphereObject< 3 >( object );
+    objects[ i ]->id = pid;
+    objects[ i ]->radius = 0.5 / npoints;
+  }
 
-	Point min_point(*points[0]);
-	Point max_point(*points[0]);
-	Point mid_point;
-  SphereObject<3> mid_object;
+  Point min_point( *points[ 0 ] );
+  Point max_point( *points[ 0 ] );
+  Point mid_point;
+  SphereObject< 3 > mid_object;
 
-	min_point.id = 0;
-	max_point.id = 0;
-	mid_point.id = 0;
+  min_point.id = 0;
+  max_point.id = 0;
+  mid_point.id = 0;
 
-	for (std::size_t i = 0; i < npoints; i++) {
-		for (std::size_t j = 0; j < 3; j++) {
-			if (min_point[j] > (*points[i])[j]) min_point[j] = (*points[i])[j];
-			if (max_point[j] < (*points[i])[j]) max_point[j] = (*points[i])[j];
-		}
-	}
+  for ( std::size_t i = 0; i < npoints; i++ ) {
+    for ( std::size_t j = 0; j < 3; j++ ) {
+      if ( min_point[ j ] > ( *points[ i ] )[ j ] )
+        min_point[ j ] = ( *points[ i ] )[ j ];
+      if ( max_point[ j ] < ( *points[ i ] )[ j ] )
+        max_point[ j ] = ( *points[ i ] )[ j ];
+    }
+  }
 
-	for (std::size_t i = 0; i < Dim; i++) {
-		mid_point.coord[i] = (max_point[i] + min_point[i]) / 2.00;
-    mid_object.coord[i] = (max_point[i] + min_point[i]) / 2.00;
-	}
+  for ( std::size_t i = 0; i < Dim; i++ ) {
+    mid_point.coord[ i ] = ( max_point[ i ] + min_point[ i ] ) / 2.00;
+    mid_object.coord[ i ] = ( max_point[ i ] + min_point[ i ] ) / 2.00;
+  }
 
-  mid_object.radius = 0.5/npoints;
+  mid_object.radius = 0.5 / npoints;
 
-	// Output data Info
-	Point & search_point = mid_point;
-  SphereObject<3> & search_object = mid_object;
+  // Output data Info
+  Point &search_point = mid_point;
+  SphereObject< 3 > &search_object = mid_object;
 
-	std::size_t numsearch = 1000000;
-	std::size_t numsearch_nearest = numsearch * 10;
+  std::size_t numsearch = 1000000;
+  std::size_t numsearch_nearest = numsearch * 10;
 
-	std::cout << " min point : " << min_point << std::endl;
-	std::cout << " max point : " << max_point << std::endl;
-	std::cout << " search_point : " << search_point << std::endl;
-	std::cout << " search radius : " << Radius << std::endl;
-	std::cout << std::endl;
+  std::cout << " min point : " << min_point << std::endl;
+  std::cout << " max point : " << max_point << std::endl;
+  std::cout << " search_point : " << search_point << std::endl;
+  std::cout << " search radius : " << Radius << std::endl;
+  std::cout << std::endl;
 
-	std::cout << " Number of Points : " << npoints << std::endl;
-	std::cout << " Number of Repetitions : " << numsearch << std::endl;
-	std::cout << std::endl;
+  std::cout << " Number of Points : " << npoints << std::endl;
+  std::cout << " Number of Repetitions : " << numsearch << std::endl;
+  std::cout << std::endl;
 
-	std::cout << "SS\t\tGEN\tSIROMP\tSIRSER\tSNPOMP\tSNPSER\tNOFR\tNP" << std::endl;
+  std::cout << "SS\t\tGEN\tSIROMP\tSIRSER\tSNPOMP\tSNPSER\tNOFR\tNP" << std::endl;
 
-	// Data Setup
-	Point * allPoints = new Point[numsearch];
-  SphereObject<3> * allSpheres = new SphereObject<3>[numsearch];
+  // Data Setup
+  Point *allPoints = new Point[ numsearch ];
+  SphereObject< 3 > *allSpheres = new SphereObject< 3 >[ numsearch ];
 
-	std::size_t max_results = npoints;
-	for (std::size_t i = 0; i < 1; i++) {
-		allPoints[i] = search_point;
-    allSpheres[i] = search_object;
-	}
+  std::size_t max_results = npoints;
+  for ( std::size_t i = 0; i < 1; i++ ) {
+    allPoints[ i ] = search_point;
+    allSpheres[ i ] = search_object;
+  }
 
-	//Prepare the search point, search radius and resut arrays
+  // Prepare the search point, search radius and resut arrays
 
-  std::vector<Entities::PtrObjectType> objectResults(max_results);
-  std::vector<double> resultDistances(max_results);
+  std::vector< Entities::PtrObjectType > objectResults( max_results );
+  std::vector< double > resultDistances( max_results );
 
-	double * distances = new double[npoints];
-	Entities::PointIterator p_results = new Entities::PtrPointType[max_results];
+  double *distances = new double[ npoints ];
+  Entities::PointIterator p_results = new Entities::PtrPointType[ max_results ];
 
-	// Point-Based Search Structures
-	std::vector<Point> points_vector;
-	for (std::size_t i = 0; i < npoints; i++) {
-		points_vector.push_back(*(points[i]));
-	}
+  // Point-Based Search Structures
+  std::vector< Point > points_vector;
+  for ( std::size_t i = 0; i < npoints; i++ ) {
+    points_vector.push_back( *( points[ i ] ) );
+  }
 
   // Point Interfaces
   // - New Interface
-  PointsNew::RunTests<PointsBins<Point>>("PointBins", points_vector, search_point, Radius, numsearch, numsearch_nearest);
+  PointsNew::RunTests< PointsBins< Point > >( "PointBins", points_vector, search_point, Radius,
+                                              numsearch, numsearch_nearest );
 
-  // - Old Interface
+// - Old Interface
 #ifdef USE_KRATOS
-	PointsOld::RunTests<Containers::BinsStaticType>("StaticBins", points, points + npoints, p_results, resultDistances.begin(), max_results, allPoints, Radius, numsearch, 1);
-	PointsOld::RunTests<Containers::BinsDynamicType>("DynamicBins", points, points + npoints, p_results, resultDistances.begin(), max_results, allPoints, Radius, numsearch, 1);
-  PointsOld::RunTests<Containers::OctreeType>("OctTree\t", points, points + npoints, p_results, resultDistances.begin(), max_results, allPoints, Radius, numsearch, 10);
-  PointsOld::RunTests<Containers::BinsDynamicOctreeType>("OcTreeDynamic\t", points, points + npoints, p_results, resultDistances.begin(), max_results, allPoints, Radius, numsearch, 10);
+  PointsOld::RunTests< Containers::BinsStaticType >( "StaticBins", points, points + npoints,
+                                                     p_results, resultDistances.begin(),
+                                                     max_results, allPoints, Radius, numsearch, 1 );
+  PointsOld::RunTests< Containers::BinsDynamicType >(
+      "DynamicBins", points, points + npoints, p_results, resultDistances.begin(), max_results,
+      allPoints, Radius, numsearch, 1 );
+  PointsOld::RunTests< Containers::OctreeType >( "OctTree\t", points, points + npoints, p_results,
+                                                 resultDistances.begin(), max_results, allPoints,
+                                                 Radius, numsearch, 10 );
+  PointsOld::RunTests< Containers::BinsDynamicOctreeType >(
+      "OcTreeDynamic\t", points, points + npoints, p_results, resultDistances.begin(), max_results,
+      allPoints, Radius, numsearch, 10 );
 #endif
 
-  // Object Interfaces
-  // - New Interface
-  // TO BE FILLED
+// Object Interfaces
+// - New Interface
+// TO BE FILLED
 
-  // - Old Interface
+// - Old Interface
 #ifdef USE_KRATOS
-  ObjectsOld::RunTests<Containers::BinsObjectStaticType>("StaticObjects", objects.begin(), objects.end(), objectResults.begin(), resultDistances.begin(), max_results, allSpheres, Radius, numsearch, 1);
-  ObjectsOld::RunTests<Containers::BinsObjectDynamicType>("DynamicObjects", objects.begin(), objects.end(), objectResults.begin(), resultDistances.begin(), max_results, allSpheres, Radius, numsearch, 1);
+  ObjectsOld::RunTests< Containers::BinsObjectStaticType >(
+      "StaticObjects", objects.begin(), objects.end(), objectResults.begin(),
+      resultDistances.begin(), max_results, allSpheres, Radius, numsearch, 1 );
+  ObjectsOld::RunTests< Containers::BinsObjectDynamicType >(
+      "DynamicObjects", objects.begin(), objects.end(), objectResults.begin(),
+      resultDistances.begin(), max_results, allSpheres, Radius, numsearch, 1 );
 #endif
   // RunTestsOldInterface<BinsObjectDynamicType>
 
-	return 0;
+  return 0;
 }
 
-int main(int arg, char* argv[]) {
-	std::string filename;
+int main( int arg, char *argv[] ) {
+  std::string filename;
 
-	// Default filename
-	filename = "../cases/genericCube2x2x2.500000.pts";
-	// filename = "../cases/genericCube100x100x100.5051.pts";
-	// filename = "../cases/randomCube2000000.pts";
+  // Default filename
+  filename = "../cases/genericCube2x2x2.500000.pts";
+  // filename = "../cases/genericCube100x100x100.5051.pts";
+  // filename = "../cases/randomCube2000000.pts";
 
-	// Default radius
-	double radius = 0.0102;
+  // Default radius
+  double radius = 0.0102;
 
-	if (arg > 1) {
-	  if ( !strncasecmp( argv[ 1], "-h", 2) || !strncasecmp( argv[ 1], "--h", 2)) {
-	    std::cout << "Usage: " << argv[ 0] << " [ filename [ radius ] ]" << std::endl;
-	    std::cout << "       filename default value = " << filename << std::endl;
-	    std::cout << "       radius default value   = " << radius << std::endl;
-	    return 0;
-	  }
-	  filename = argv[1];
-	  if (arg == 3) {
-	    radius = atof(argv[2]) / 1000000;
-	  }
-	}
-	
-	RunPointSearchComparison(filename, radius);
-	
-	// filename = "../cases/offsetCube79x79x79.1603.pts";
-	// RunPointSearchComparison(filename, radius);
-	// filename = "../cases/clusterCube6x6x6X4913.490.pts";
-	// RunPointSearchComparison(filename, radius);
-	// filename = "../cases/line100000.5.pts";
-	// RunPointSearchComparison(filename, radius);
+  if ( arg > 1 ) {
+    if ( !strncasecmp( argv[ 1 ], "-h", 2 ) || !strncasecmp( argv[ 1 ], "--h", 2 ) ) {
+      std::cout << "Usage: " << argv[ 0 ] << " [ filename [ radius ] ]" << std::endl;
+      std::cout << "       filename default value = " << filename << std::endl;
+      std::cout << "       radius default value   = " << radius << std::endl;
+      return 0;
+    }
+    filename = argv[ 1 ];
+    if ( arg == 3 ) {
+      radius = atof( argv[ 2 ] ) / 1000000;
+    }
+  }
 
-	return 0;
+  RunPointSearchComparison( filename, radius );
+
+  // filename = "../cases/offsetCube79x79x79.1603.pts";
+  // RunPointSearchComparison(filename, radius);
+  // filename = "../cases/clusterCube6x6x6X4913.490.pts";
+  // RunPointSearchComparison(filename, radius);
+  // filename = "../cases/line100000.5.pts";
+  // RunPointSearchComparison(filename, radius);
+
+  return 0;
 }
